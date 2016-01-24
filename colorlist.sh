10fd9acbf3932632d135fa30d5df5e046530706f
mkaminaga <mkaminaga2.718@gmai.com>
2016-01-24 13:45:48 +0900

    comment placement changed

--- colorlist.sh
+++ colorlist.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# draw color
+# draw sample 256 color num with coresponding forground
 for i in {0..255}; do
   str=`printf "%03d" $i`
 
-  # draw sample 256 color num with coresponding forground
   printf " \e[38;05;${i}m${str}\e[m"
 
   if [ $((${i} % 16)) -eq 15 ];then

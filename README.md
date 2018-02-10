10148a542518f211caac8aaaef32b8c078f615ef
mkaminaga <mkaminaga2.718@gmail.com>
2018-02-10 21:15:51 +0900

    Right hand and left hand version is added

--- README.md
+++ README.md
@@ -21,6 +21,8 @@ gnuplotなどのデータファイルを弄る際に用いることを想定。<
 詳しくは'-help'オプションを参照。
 
 [touch\_type\_practice.sh](touch_type_practice.sh)
+[touch\_type\_practice\_left.sh](touch_type_practice_left.sh)
+[touch\_type\_practice\_right.sh](touch_type_practice_right.sh)
 ----
 タッチタイプの練習用<br>
 小文字アルファベット＋数字＋記号一文字がランダム出現<br>
---
+++ touch_type_practice_right.sh
@@ -0,0 +1,98 @@
+﻿#!/bin/bash
+
+declare -a CHAR_LIST=( \
+  'h' 'i' 'j' 'k' 'l' 'm' 'n' 'o' 'p' 'u' \
+  'y' '0' '6' '7' '8' '9' ',' '.' '/' ';' \
+  ':' '@' '[' ']' '-' '^')
+declare -a ASK_CNT
+declare -a ERR_CNT
+declare -i LOOP_END=0
+
+handler() {
+  LOOP_END=1
+
+  echo ""
+  echo '==== Result ===='
+  declare -i total_ask=0
+  declare -i total_err=0
+  local err_rate=0
+  echo 'no/character/count/error'
+  for i in $(seq 0 $((${#CHAR_LIST[@]} - 1))); do
+    # Show
+    if [[ ${ASK_CNT[${i}]} -ne 0 ]]; then
+      err_rate=$(echo "scale=20; ${ERR_CNT[${i}]}/${ASK_CNT[${i}]} * 100.0" | bc)
+      err_rate=$(printf '%.1f' ${err_rate})
+    else
+      err_rate=0
+    fi
+    echo "${i} ${CHAR_LIST[${i}]} ${ASK_CNT[${i}]} ${err_rate}%"
+    # Sum
+    total_ask=$((${total_ask} + ${ASK_CNT[${i}]}))
+    total_err=$((${total_err} + ${ERR_CNT[${i}]}))
+  done
+  if [[ ${total_ask} -ne 0 ]]; then
+    err_rate=$(echo "scale=20; ${total_err}/${total_ask} * 100.0" | bc)
+    err_rate=$(printf '%.1f' ${err_rate})
+  else
+    err_rate=0
+  fi
+
+  echo '---- Total Result ----'
+  echo "asked count : ${total_ask}"
+  echo "error count : ${total_err}"
+  echo "error rate : ${err_rate}%"
+  echo "score : ${score}"
+
+  # Self terminate
+  kill -9 $$
+}
+trap handler 0 1 2 3 15
+
+main() {
+  declare -i missed=0
+  declare -i no=0
+  declare -i id=0
+  declare -i score=0
+  local c=0
+  local cin=0
+
+  # Reserve array
+  for i in $(seq 0 $((${#CHAR_LIST[@]} - 1))); do
+    ASK_CNT+=('0')
+    ERR_CNT+=('0')
+  done
+
+  echo 'Practice start.'
+
+  while [[ ${LOOP_END} -eq 0 ]]; do
+    if [[ ${missed} -ne 1 ]]; then
+      id=$(($RANDOM % ${#CHAR_LIST[@]}))
+      miss=0
+    fi
+    c=${CHAR_LIST[${id}]}
+    echo "No.${no}: ${c}"
+    read -s -n1 cin
+    # The asked num is counted
+    ASK_CNT[${id}]=$((${ASK_CNT[${id}]} + 1))
+    # Judge
+    if [[ ${cin} = ${c} ]]; then
+      missed=0
+      echo 'Good.'
+      echo "score=${score}"
+      echo ""
+      score=$((${score} + 1))
+    else
+      missed=1
+      echo '*=*=*=*=* Not that! XD *=*=*=*=*'
+      echo "score=${score}"
+      echo ""
+      score=$((${score} - 10))
+      # The error is counted
+      ERR_CNT[${id}]=$((${ERR_CNT[${id}]} + 1))
+    fi
+    no=$((${no} + 1))
+  done
+
+  echo 'Practice stop.'
+}
+main
